d50d320967ddef8360b3ce0e068f3370b0c7d365
CharlesEmmanuelVanier <charles-emmanuel.vanier.1@ens.etsmtl.ca>
2025-01-28 11:40:48 -0500

    Renommer les fonctions et variable pour respecter les normes vues en classe

--- JeuDuPendu.py
+++ JeuDuPendu.py
@@ -1,90 +1,89 @@
 import random
 import unicodedata
-import string
 import os.path
 
 def strip_accent(s):
     return ''.join(c for c in unicodedata.normalize('NFD', s)
                    if unicodedata.category(c) != 'Mn')
 
-def GetRandom(Filename):
-    File = open(Filename, "r", encoding="utf8")
-    string = File.readlines()
+def get_random(Filename):
+    file = open(Filename, "r", encoding="utf8")
+    string = file.readlines()
     return strip_accent(random.choice(string))
 
-def GetIndice(GameName,GuessHistory):
-    Alphabet_String = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
-    Alphabet_List = Alphabet_String.split(",")
-    GoodIndice = 0
-    while GoodIndice == 0:
-        Indice = random.choice(Alphabet_List)
-        GoodIndice = 1
+def get_indice(GameName,GuessHistory):
+    alphabetString = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+    alphabetList = alphabetString.split(",")
+    goodIndice = 0
+    while goodIndice == 0:
+        indice = random.choice(alphabetList)
+        goodIndice = 1
         for i in range(len(GameName)):
-            if Indice == GameName[i]:
-                GoodIndice = 0
+            if indice == GameName[i]:
+                goodIndice = 0
         for i in range(len(GuessHistory)):
-            if Indice == GuessHistory[i]:
-                GoodIndice = 0
+            if indice == GuessHistory[i]:
+                goodIndice = 0
 
-    return Indice
+    return indice
 
 
-def CheckWin(VerifWin):
-    for i in range(len(VerifWin)):
-        if(VerifWin[i] == 0):
+def check_win(verifWin):
+    for i in range(len(verifWin)):
+        if(verifWin[i] == 0):
             return 0
     return 1
 
-def JeuDuPendu(Filename):
-    NbreChances = 6
-    NameChosen = GetRandom(Filename).strip()
-    VerifName = []
-    GuessHistory = []
-    print(NameChosen)
-    for i in range(len(NameChosen)):
-        VerifName.append(0)
+def jeu_du_pendu(filename):
+    nbreChances = 6
+    nameChosen = get_random(filename).strip()
+    verifName = []
+    guessHistory = []
+    print(nameChosen)
+    for i in range(len(nameChosen)):
+        verifName.append(0)
 
-    while(NbreChances > 0):
+    while(nbreChances > 0):
         #Affichage de l'état du mot à deviné
         print("\nVoici l'état du mot à deviner:")
-        for i in range(len(NameChosen)):
-            if VerifName[i] == 1:
-                print(NameChosen[i],end="")
+        for i in range(len(nameChosen)):
+            if verifName[i] == 1:
+                print(nameChosen[i],end="")
             else:
                 print(" _ ",end="")
 
         #Interaction avec le joueur pour demander une lettre
-        Guess = input("\n Veuillez entrer une lettre de l'alphabet\n")
-        GuessHistory.append(Guess)
+        guess = input("\n Veuillez entrer une lettre de l'alphabet\n")
+        guessHistory.append(guess)
 
         #Vérification si la lettre est dans le mot
-        GoodGuess = len(NameChosen)
-        for i in range(len(NameChosen)):
-            if(NameChosen[i] == Guess):
-                VerifName[i] = 1
+        goodGuess = len(nameChosen)
+        for i in range(len(nameChosen)):
+            if(nameChosen[i] == guess):
+                verifName[i] = 1
             else:
-                GoodGuess -= 1 #À la fin de la boucle il me reste le nombre de bon guess effectué
+                goodGuess -= 1 #À la fin de la boucle il me reste le nombre de bon guess effectué
         #Affichage du résultat du guess, si la lettre était dans le mot ou pas.
-        if(GoodGuess > 0):
-            print(f"Félicitation! Il y a {GoodGuess} '{Guess}' dans le mot.")
+        if(goodGuess > 0):
+            print(f"Félicitation! Il y a {goodGuess} '{guess}' dans le mot.")
             #Petite fonction qui détermine si le joueur a deviner le mot en entier ou s'il reste des lettres à deviner
-            if(CheckWin(VerifName)):
-                print(f"VICTOIRE !\nFélicitation vous avez gagné!\nLe mot était '{NameChosen}'")
+            if(check_win(verifName)):
+                print(f"VICTOIRE !\nFélicitation vous avez gagné!\nLe mot était '{nameChosen}'")
                 return 1
         else:
-            NbreChances -= 1
-            print(f"Malheuresement il n'y a pas de '{Guess}' dans le mot\n Vous perdez une chance.")
-            print(f"Il vous reste {NbreChances} chances")
-            if (NbreChances == 1):
+            nbreChances -= 1
+            print(f"Malheuresement il n'y a pas de '{guess}' dans le mot\n Vous perdez une chance.")
+            print(f"Il vous reste {nbreChances} chances")
+            if (nbreChances == 1):
                 if (input("\nIl ne vous reste plus qu'une seule vie !\n Voulez-vous un indice ? (o/n) ") == "o"):
-                    print(f"\n\nCette lettre ne fait pas partie du mot: {GetIndice(NameChosen, GuessHistory)}")
+                    print(f"\n\nCette lettre ne fait pas partie du mot: {get_indice(nameChosen, guessHistory)}")
                 #Partie terminé lorsque le joueur n'a plus de chance.
-                elif NbreChances == 0:
+                elif nbreChances == 0:
                     print("\n#####################\nVous n'avez plus de chance !\nPartie terminée\n#####################\n")
-                    print(f"Le mot était {NameChosen}")
+                    print(f"Le mot était {nameChosen}")
                     return 0
 
-def ChangementFile():
+def changement_filename():
     print("À noter que le format du fichier devra être le suivant:\n"
           "mot1\n"
           "mot2\n"
@@ -97,13 +96,16 @@ def menu():
           "Voulez-vous utiliser une liste de mot personnalisé? (o/n) ")
 
     if(customFile == "o"):
-        Filename = ChangementFile()
+        filename = changement_filename()
+        if(not os.path.exists(filename)):
+            print("Fichier introuvable, Arrêt du programme")
+            quit()
     else:
-        Filename = "names.txt"
+        filename = "names.txt"
 
     #Lancement de la première partie
-    result = JeuDuPendu(Filename)
-    #Demande au joueur s'il veut rejouer, jusqu'à ce qu'il ne veule plus.
+    result = jeu_du_pendu(filename)
+    #Demande au joueur s'il veut rejouer, jusqu'à ce qu'il dise non.
     choix = "o"
     while(choix == "o"):
         if result == 1:
@@ -111,8 +113,9 @@ def menu():
         else:
             choix = input("Voulez vous votre revanche ?\n Rejouer ? (o/n) ")
         if choix == "o":
-            result = JeuDuPendu()
+            result = jeu_du_pendu(filename)
         else:
-            print("\n\n\nÀ bientot!")
+            return "\n\n\nÀ bientot !\n\n"
 
-menu()
+#Appelle la fonction principale du programme
+print(menu())
